451f0f97c666055d2d0bd6d6463d34940c3b395f
tyub1 <133926177+tyubi@users.noreply.github.com>
2025-02-15 10:39:25 +0800

    update sa cont and router coordinator

--- backend/server/routes/coordinator.js
+++ backend/server/routes/coordinator.js
@@ -1,13 +1,13 @@
 import express from 'express'; 
-import { getCoordinators, getCoordinatorById, addCoordinator, updateCoordinator, deleteCoordinator } from '../controllers/coordinatoController.js';
+import { getCoordinators, getCoordinatorById, addCoordinator, updateCoordinator, deleteCoordinator } from '../controllers/coordinatorController.js';
 
 const router = express.Router();
 
-router.get('/getCoordinators', getCoordinators);
+// Change this line to match what Navbar is calling
+router.get('/getCoordinator', getCoordinators);
 router.get('/getCoordinators/:id', getCoordinatorById);
 router.put('/updateCoordinator/:id', updateCoordinator);
 router.post('/addCoordinator', addCoordinator);
 router.delete('/deleteCoordinator/:id', deleteCoordinator);
 
-
-export default router; 
+export default router;
